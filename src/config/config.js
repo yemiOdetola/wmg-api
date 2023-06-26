e446674aedbc7046164cfa7ba1b72fbc70ac3b85
Odetola Azeez Opeyemi <yemiotola@gmail.com>
2023-06-26 19:03:08 +0100

    wip

--- src/config/config.js
+++ src/config/config.js
@@ -52,12 +52,17 @@ module.exports = {
   },
   email: {
     smtp: {
-      host: envVars.SMTP_HOST,
-      port: envVars.SMTP_PORT,
+      // host: envVars.SMTP_HOST,
+      // port: envVars.SMTP_PORT,
+      // auth: {
+      //   user: envVars.SMTP_USERNAME,
+      //   pass: envVars.SMTP_PASSWORD,
+      // },
+      service: 'gmail',
       auth: {
         user: envVars.SMTP_USERNAME,
-        pass: envVars.SMTP_PASSWORD,
-      },
+        pass: envVars.SMTP_PASSWORD
+      }
     },
     from: envVars.EMAIL_FROM,
   },
